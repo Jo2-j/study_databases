73348bfe68323657a834ca1c27dd5b8610fb4d85
Jo2-j <w01205250623@nate.com>
2024-10-16 08:20:38 +0000

    categories

---
+++ sqls/sqls/CategoriesDescWithProducts.sql
@@ -0,0 +1,15 @@
+-- - Table : Products
+-- - 조건 : 카테고리에 속한 상품 갯수 순으로 Desc
+
+
+-- 상품이름, 상품갯수
+
+
+SELECT C.CategoryName, COUNT(P.ProductName)
+FROM 
+Categories as C INNER JOIN Products as P
+	                ON C.CategoryID = P.CategoryID
+                 
+GROUP BY C.CategoryID
+ORDER BY COUNT(P.ProductName)
+;
--- sqls/sqls/OrdersMaxEmployee.sql
+++ sqls/sqls/OrdersMaxEmployee.sql
@@ -16,3 +16,16 @@ GROUP BY SupplierName
 ORDER BY COUNT(SupplierName) desc
 LIMIT 1
 ;
+
+-- 아래 한번 맛보기
+
+SELECT EmployeeID, count(OrderID) AS CNT
+FROM Orders
+GROUP BY EmployeeID
+
+SELECT OrderTb.CNT, Employees.FirstName
+FROM (SELECT EmployeeID, count(OrderID) AS CNT
+FROM Orders
+GROUP BY EmployeeID) as OrderTb INNER JOIN Employees ON OrderTb.EmployeeID = Employees.EmployeeID
+ORDER BY OrderTb.CNT
+;
