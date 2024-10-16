264a26c317734e743d1dc5f2fd953a60e46df5e4
Jo2-j <w01205250623@nate.com>
2024-10-16 07:35:07 +0000

    edit

--- sqls/CustomersOrderInformations.sql
+++ sqls/CustomersOrderInformations.sql
@@ -7,12 +7,23 @@
 -- Products - COUNT(ProductName)
 
 
-SELECT ProductName, Price, CustomerName, COUNT(ProductName)
-FROM ((((Customers left join Orders
-	ON Customers.CustomerID = Orders.CustomerID)
-	left join OrderDetails
-    ON Orders.OrderID = OrderDetails.OrderID))
-        left join Products
-        ON OrderDetails.ProductID = Products.ProductID)
+-- SELECT ProductName, Price, CustomerName, COUNT(ProductName 이 부분에서 틀렸음)
+-- FROM ((((Customers AS C INNER JOIN Orders AS O
+-- 	                    ON C.CustomerID = O.CustomerID)
+-- 	                        INNER JOIN OrderDetails AS OD
+--                                 ON O.OrderID = OD.OrderID))
+--                                         INNER JOIN Products AS P
+--                                                 ON OD.ProductID = P.ProductID)
+-- GROUP BY Customers.CustomerName,Products.ProductName,Products.Price
+-- ; 내code
 
-;
+-- SELECT Customers.CustomerName, Products.ProductName, Products.Price ,COUNT(Orders.OrderID)
+-- FROM Customers
+-- INNER JOIN Orders
+-- 	ON Customers.CustomerID = Orders.CustomerID
+-- INNER JOIN OrderDetails
+-- 	ON Orders.OrderID = OrderDetails.OrderID
+-- INNER JOIN Products 
+-- 	ON OrderDetails.ProductID = Products.ProductID
+-- GROUP BY Customers.CustomerName,Products.ProductName,Products.Price
+-- ; 연지님code, 테이블.컬럼 < 다음과 같은 구조로 써야지 나중에 코드 오류에 대한 신경을 덜 쓰게 됨 
--- sqls/sqls/OrdersMaxEmployee.sql
+++ sqls/sqls/OrdersMaxEmployee.sql
@@ -6,12 +6,12 @@
 --Orders - OrderDetails - Products - Suppliers
 SELECT SupplierName, COUNT(SupplierName)
 FROM 
-        ((((Orders as O INNER JOIN OrderDetails as OD
-	ON O.OrderID = OD.OrderID)
-	INNER JOIN Products as P
-    ON OD.ProductID = P.ProductID))
-        INNER JOIN Suppliers as S
-        ON P.SupplierID = S.SupplierID) 
+((((Orders as O INNER JOIN OrderDetails as OD
+	                ON O.OrderID = OD.OrderID)
+	                    INNER JOIN Products as P
+                            ON OD.ProductID = P.ProductID))
+                                INNER JOIN Suppliers as S
+                                    ON P.SupplierID = S.SupplierID)
 GROUP BY SupplierName
 ORDER BY COUNT(SupplierName) desc
 LIMIT 1
